d70afdfa8429c67377379a75e8878981c9f8e216
rares-p <panainte.rares02@gmail.com>
2023-11-15 12:17:40 +0200

    fixed small bug in Examination

--- Core/Domain/HealthcareManagementSystem.Domain/Entities/Examination.cs
+++ Core/Domain/HealthcareManagementSystem.Domain/Entities/Examination.cs
@@ -5,21 +5,23 @@ namespace HealthcareManagementSystem.Domain.Entities
 {
     public class Examination : AuditableEntity
     {
-        private Examination(DateTime date, Department department)
+        private Examination(DateTime date, Department department, Guid userId, Guid medicId)
         {
             Id = Guid.NewGuid();
             Date = date;
             Department = department;
+            UserId = userId;
+            MedicId = medicId;
         }
         public Guid Id { get; private set; }
-        public User User { get; private set; }
-        public Medic Medic { get; private set; }
+        public Guid UserId { get; private set; }
+        public Guid MedicId { get; private set; }
         public DateTime Date { get; private set; }
         public Department Department { get; private set; }
 
-        public static Result<Examination> Create(DateTime date, Department department)
+        public static Result<Examination> Create(DateTime date, Department department, Guid userId, Guid medicId)
         {
-            return Result<Examination>.Success(new Examination(date, department));
+            return Result<Examination>.Success(new Examination(date, department, userId, medicId));
         }
     }
 }
