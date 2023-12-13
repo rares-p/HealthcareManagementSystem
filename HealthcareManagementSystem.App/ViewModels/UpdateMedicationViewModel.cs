7795b21b8ff9a9e92b1706afe4421d3848748990
rares-p <panainte.rares02@gmail.com>
2023-12-13 15:14:16 +0200

    update medication

---
+++ HealthcareManagementSystem.App/ViewModels/UpdateMedicationViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HealthcareManagementSystem.App.ViewModels
+{
+    public class UpdateMedicationViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
