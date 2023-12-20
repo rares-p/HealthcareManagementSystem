ea393b334e541e3d39c0f3cf841176c765310c9a
rares-p <panainte.rares02@gmail.com>
2023-12-20 16:09:02 +0200

    edit medication

--- API/Controllers/MedicationReminderController.cs
+++ API/Controllers/MedicationReminderController.cs
@@ -6,8 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-	[ApiController]
 	public class MedicationReminderController : ApiControllerBase
 	{
 		[HttpGet]
