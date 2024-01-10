8601187047257c8447244b4884123fecefe37839
teo000 <95464824+teo000@users.noreply.github.com>
2024-01-10 14:17:52 +0200

    added ExaminationController with create and getall

---
+++ Core/Application/HealthcareManagementSystem.Application/Features/Examinations/Queries/GetAllExaminations/GetAllExaminationsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace HealthcareManagementSystem.Application.Features.Examinations.Queries.GetAllExaminations
+{
+	public class GetAllExaminationsQuery : IRequest<GetAllExaminationsResponse>
+	{
+	}
+}
---
+++ Core/Application/HealthcareManagementSystem.Application/Features/Examinations/Queries/GetAllExaminations/GetAllExaminationsQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using HealthcareManagementSystem.Application.Features.Medics.Queries.GetAll;
+using HealthcareManagementSystem.Application.Persistence;
+using HealthcareManagementSystem.Domain.Entities;
+using MediatR;
+
+namespace HealthcareManagementSystem.Application.Features.Examinations.Queries.GetAllExaminations
+{
+	public class GetAllExaminationsQueryHandler : IRequestHandler<GetAllExaminationsQuery, GetAllExaminationsResponse>
+	{
+		private readonly IExaminationRepository _repository;
+		public GetAllExaminationsQueryHandler(IExaminationRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<GetAllExaminationsResponse> Handle(GetAllExaminationsQuery request, CancellationToken cancellationToken)
+		{
+			var response = new GetAllExaminationsResponse();
+			var result = await _repository.GetAllAsync();
+
+			if (result.IsSuccess)
+			{
+				response.Examinations = result.Value.Select(examination => new ExaminationDto()
+				{
+					Id = examination.Id,
+					UserId = examination.UserId,
+					MedicId = examination.MedicId,
+					Department = examination.Department.ToString(),
+					Date = examination.Date
+				}).ToList();
+			}
+
+			return response;
+		}
+	}
+}
