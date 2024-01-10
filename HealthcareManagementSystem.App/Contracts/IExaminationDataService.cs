271bf2c0c9d95b07cd94a3843815fee695052bf6
teo000 <95464824+teo000@users.noreply.github.com>
2024-01-10 15:44:55 +0200

    refacut niste chestii

--- HealthcareManagementSystem.App/Contracts/IExaminationDataService.cs
+++ HealthcareManagementSystem.App/Contracts/IExaminationDataService.cs
@@ -1,6 +1,13 @@
-﻿namespace HealthcareManagementSystem.App.Contracts
+﻿using HealthcareManagementSystem.App.Services.Responses;
+using HealthcareManagementSystem.App.ViewModels;
+
+namespace HealthcareManagementSystem.App.Contracts
 {
 	public interface IExaminationDataService
 	{
-	}
+        Task<List<ExaminationViewModel>> GetExaminationsAsync();
+        Task<ApiResponse<ExaminationViewModel>> CreateExaminationAsync(CreateExaminationViewModel examinationViewModel);
+
+
+    }
 }
--- HealthcareManagementSystem.App/ViewModels/ExaminationViewModel.cs
+++ HealthcareManagementSystem.App/ViewModels/ExaminationViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace HealthcareManagementSystem.App.ViewModels
+﻿using HealthcareManagementSystem.Domain.Data;
+
+namespace HealthcareManagementSystem.App.ViewModels
 {
 	public class ExaminationViewModel
 	{
-	}
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public Guid MedicId { get; private set; }
+        public DateTime Date { get; private set; }
+        public Department Department { get; private set; }
+    }
 }
